68fbe14072c29cf6b9cff431f04486cfb6cc071f
dkengineering0 <dmytro.kyryienko.engineer@gmail.com>
2023-12-08 08:43:29 +0300

    8

--- solutions/8/first.rb
+++ solutions/8/first.rb
@@ -3,9 +3,27 @@
 module Day8
   class Part1 < BaseSolution
     def run
+      dirs, _, *nodes = chomped_lines
+      dirs = dirs.chars.cycle
+      node_map = nodes.map do |node|
+        k, v = node.split(' = ')
+        l, r = v.gsub(/[()]/, '').split(', ')
+        [k, {'L' => l, 'R' => r}]
+      end.to_h
+
+      current = 'AAA'
+      steps = 0
+
+      until current == 'ZZZ'
+        steps += 1
+        current = node_map[current][dirs.next]
+      end
+
+      steps
     end
 
     def self.expected_result
+      2
     end
   end
 end
--- solutions/8/second.rb
+++ solutions/8/second.rb
@@ -3,8 +3,30 @@
 module Day8
   class Part2 < BaseSolution
     def run
+      dirs, _, *nodes = chomped_lines
+      dirs = dirs.chars.cycle
+      node_map = nodes.map do |node|
+        k, v = node.split(' = ')
+        l, r = v.gsub(/[()]/, '').split(', ')
+        [k, {'L' => l, 'R' => r}]
+      end.to_h
+
+      starts = node_map.keys.select { |k| k.end_with?('A') }
+
+      starts.reduce(1) do |acc, current|
+        steps = 0
+
+        until current.end_with?('Z')
+          steps += 1
+          current = node_map[current][dirs.next]
+        end
+
+        acc.lcm(steps)
+      end
     end
+
     def self.expected_result
+      6
     end
   end
 end
