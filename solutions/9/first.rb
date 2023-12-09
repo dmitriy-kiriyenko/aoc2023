0611eecbfdb1851c15debf0cdb461083251b9fb0
dkengineering0 <dmytro.kyryienko.engineer@gmail.com>
2023-12-09 08:21:50 +0300

    9

--- solutions/9/first.rb
+++ solutions/9/first.rb
@@ -3,9 +3,25 @@
 module Day9
   class Part1 < BaseSolution
     def run
+      extrapolated = chomped_lines.map do |line|
+        numbers = line.split(' ').map(&:to_i)
+
+        differences = []
+        difference = numbers
+
+        until difference.all?(&:zero?)
+          difference = difference.each_cons(2).map { |(l, r)| r-l }
+          differences << difference
+        end
+
+        differences.map(&:last).sum + numbers.last
+      end
+
+      extrapolated.sum
     end
 
     def self.expected_result
+      114
     end
   end
 end
--- solutions/9/second.rb
+++ solutions/9/second.rb
@@ -3,8 +3,25 @@
 module Day9
   class Part2 < BaseSolution
     def run
+      extrapolated = chomped_lines.map do |line|
+        numbers = line.split(' ').map(&:to_i)
+
+        differences = []
+        difference = numbers
+
+        until difference.all?(&:zero?)
+          difference = difference.each_cons(2).map { |(l, r)| r-l }
+          differences << difference
+        end
+
+        numbers.first - differences.reverse.map(&:first).reduce(0) { |acc, i| i - acc }
+      end
+
+      extrapolated.sum
     end
+
     def self.expected_result
+      2
     end
   end
 end
